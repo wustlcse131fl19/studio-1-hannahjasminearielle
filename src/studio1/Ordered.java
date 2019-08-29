cce05220a7c5ac4e13a0c86d2db0001bc98deef2
hannahginsberg <h.ginsberg@wustl.edu>
2019-08-29 12:11:52 -0500

    done

--- src/studio1/Ordered.java
+++ src/studio1/Ordered.java
@@ -8,10 +8,19 @@ public class Ordered {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+
+		boolean isOrdered = true;
+
 		ArgsProcessor ap = new ArgsProcessor(args);
 		int x = ap.nextInt("Value for x?");
 		int y = ap.nextInt("Value for y?");
 		int z = ap.nextInt("Value for z?");
+
+		isOrdered = (x > y && y > z) || (x < y && y < z);
+
+		System.out.println(isOrdered);
+
+
 
 	}
 
